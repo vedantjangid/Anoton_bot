77ba7a08721408a24ffb384f0689b62b926a0707
Vedant Jangid <77989366+vedantjangid@users.noreply.github.com>
2021-11-20 00:45:10 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/iamLiquidX/MirLeechX")
-    buttons.buildbutton("Group", "https://t.me/mirrorLeechTelegramBot")
+    buttons.buildbutton("Owner", "hhttps://t.me/ANTON6999")
+    buttons.buildbutton("Group", "https://t.me/joinchat/YH4an_2giq9iYzM1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
